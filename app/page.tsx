b5987d890a897593ff1db52f98ce2915e62c1852
runsdev <harunsixsixfour@gmail.com>
2026-06-01 16:40:40 +0700

    refactor: replace async translation fetching with useTranslations hook and simplify component structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
 import { Suspense } from 'react';
 
-import dynamic from 'next/dynamic';
-
+import BelowFoldSections from './components/BelowFoldSections';
 import BottomNav from './components/BottomNav';
 import HeroSection from './components/HeroSection';
 import JsonLd from './components/JsonLd';
 import QuizAccordionSection from './components/QuizAccordionSection';
 import StripSearchParams from './components/StripSearchParams';
 import TopAppBar from './components/TopAppBar';
-
-// Below-fold sections: lazy-loaded so they don't block LCP / initial paint
-const PurposeSection = dynamic(() => import('./components/PurposeSection'), { ssr: false });
-const BentoGrid = dynamic(() => import('./components/BentoGrid'), { ssr: false });
-const AboutSection = dynamic(() => import('./components/AboutSection'), { ssr: false });
 
 export default function Home() {
   return (
@@ -25,11 +19,7 @@ export default function Home() {
       <main style={{ paddingTop: 80, paddingBottom: 68 }} className="md:pb-0">
         <HeroSection />
         <QuizAccordionSection />
-        <Suspense fallback={null}>
-          <PurposeSection />
-          <BentoGrid />
-          <AboutSection />
-        </Suspense>
+        <BelowFoldSections />
       </main>
       <BottomNav />
     </>
